4e3bea908c216656aa5b1be445e3a789260b194b
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-06-08 01:30:09 +0600

    implement patch operation for instructor

--- index.js
+++ index.js
@@ -83,6 +83,23 @@ async function run() {
         })
 
 
+        // instructor
+        
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDocument = {
+                $set: {
+                    role:'instructor'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDocument);
+            res.send(result);
+
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
