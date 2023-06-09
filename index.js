ff54f6a31564fb2072e98df4c1242ec3b1dcf776
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-06-09 15:26:08 +0600

    add jwt and verify jwt

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 const cors = require('cors')
 // middleware
 app.use(cors());
 app.use(express.json());
 
+
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
 
 
 
@@ -35,11 +52,58 @@ async function run() {
         const usersCollection = client.db('summerCampDB').collection('users');
         const classCollection = client.db('summerCampDB').collection('classes');
 
+
+        // jwt
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token });
+        })
+
+
         //   classes
+        app.get('/classes', async (req, res) => {
+            const result = await classCollection.find().toArray();
+            res.send(result);
+        })
+
+
+        app.get('/classes/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+
+            }
+            const query = { instructorEmail: email }
+            const result = await classCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.post('/classes', async (req, res) => {
             const myClass = req.body;
             const result = await classCollection.insertOne(myClass);
             res.send(result);
+        })
+
+        app.patch('/classes/:id', async (req, res) => {
+            const { i } = req.body;
+            console.log(i);
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+
+            const updateDocument = {
+                $set: {
+                    status: i === true ? 'approved' : 'denied'
+                }
+
+
+            }
+
+
+            const result = await classCollection.updateOne(filter, updateDocument);
+            res.send(result);
+
         })
 
 
@@ -94,8 +158,12 @@ async function run() {
 
         // instructor
 
-        app.get('/users/instructor/:email', async (req, res) => {
+        app.get('/users/instructor/:email',verifyJWT, async (req, res) => {
             const email = req.params.email;
+            if (req.decoded.email !== email) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+
+            }
             const query = { email: email };
             const user = await usersCollection.findOne(query);
             const result = { instructor: user?.role === 'instructor' };
